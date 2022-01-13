36cf4a38ef208f293759c28bcf0f1ba09d2ad7cc
denisugo <superden1320@gmail.com>
2022-01-13 11:41:05 +0100

    Update database connection settings

--- db/index.js
+++ db/index.js
@@ -2,11 +2,12 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  sqlPort: process.env.SQL_PORT,
-  sqlHost: process.env.SQL_HOST,
-  database: process.env.SQL_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  // user: process.env.SQL_USER,
+  // password: process.env.SQL_PASSWORD,
+  // sqlPort: process.env.SQL_PORT,
+  // sqlHost: process.env.SQL_HOST,
+  // database: process.env.SQL_DATABASE,
 });
 
 module.exports = pool;
